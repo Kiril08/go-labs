0c313fe06a7aa9255bac1310a40314d6d49986b3
konstantin-losevich <kanstantsin.lasevich@finteqhub.com>
2026-04-29 11:26:55 +0200

    add task-02

---
+++ task-02/main.go
@@ -0,0 +1,5 @@
+package main
+
+//1. Countdown
+//Write a program that prints numbers from 10 down to 1, and then prints "Blast off!" at the end.
+//Goal: Practice using decrement (i--) and the loop termination condition.
