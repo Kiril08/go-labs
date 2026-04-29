809b71b2f9daf008975c5d5f8d9ccb68ca4e73f6
Kiril08 <kirilllosevic.com>
2026-04-29 14:02:32 +0300

    Add task_02

--- task-02/main.go
+++ task-02/main.go
@@ -1,5 +1,24 @@
 package main
 
-//1. Countdown
-//Write a program that prints numbers from 10 down to 1, and then prints "Blast off!" at the end.
-//Goal: Practice using decrement (i--) and the loop termination condition.
+import (
+	"fmt"
+)
+
+func task_02_1() {
+	for i := 10; i >= 1; i-- {
+		fmt.Printf("%d\n", i)
+	}
+	fmt.Println("Blast off")
+}
+
+func main() {
+	task_02_2()
+}
+
+func task_02_2() {
+	var sum = 0
+	for t := -1; t >= -10; t-- {
+		sum = sum + t
+	}
+	fmt.Println(sum)
+}
